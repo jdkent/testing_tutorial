0c4e90cecde3d397d22c936e96b78f9b58aa1397
James Kent <james-kent@uiowa.edu>
2018-07-30 18:50:37 -0500

    change usecolumn to 1

--- demean_age.py
+++ demean_age.py
@@ -1,13 +1,13 @@
 import sys
 import numpy as np
 
-age = np.loadtxt(sys.argv[1], skiprows=1, usecols=3)
+age = np.loadtxt(sys.argv[1], skiprows=1, usecols=1)
 
 mean_age = sum(age)/len(age)
 
 np.savetxt("demeaned_" + sys.argv[1], age-mean_age)
 
-assert mean_age < 100
-assert mean_age > 10
+assert age < 100
+assert age > 10
 
 print("done!")
